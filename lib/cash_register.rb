5e1d6fe14e40733cdf91d55035125448b7532dca
Brandon Yoon <byoon5397@gmail.com>
2020-04-01 16:01:24 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    quantity.times do
+      @cart << title
+    end
+  end
+
+  def apply_discount
+    if @discount != 0
+      @total -= @total * @discount / 100
+    else
+      "There is no discount to apply."
+    end
+  end
+end
