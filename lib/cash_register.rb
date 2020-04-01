26014542973c29f6d3e03a5da1c1b768e1782134
Brandon Yoon <byoon5397@gmail.com>
2020-04-01 16:07:30 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,6 +18,7 @@ class CashRegister
   def apply_discount
     if @discount != 0
       self.total -= self.total * @discount / 100
+      return "After the discount, the total comes to $#{total}."
     else
       "There is no discount to apply."
     end
